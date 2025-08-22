29838170e53372660a858ada06b1f92e2370887d
AshedFox <a.mahn@mail.ru>
2025-08-22 18:12:27 +0300

    feat: implement username generation module

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 
+import { UsernameGeneratorModule } from './username-generator/username-generator.module';
+
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true })],
+  imports: [ConfigModule.forRoot({ isGlobal: true }), UsernameGeneratorModule],
   controllers: [],
   providers: [],
 })
