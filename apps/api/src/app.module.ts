1b3861ecb40171f6e68906eb5ea3b7b4f7d4c9f8
AshedFox <a.mahn@mail.ru>
2025-08-22 18:43:55 +0300

    feat: setup redis client

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,10 +1,21 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { RedisModule } from '@nestjs-modules/ioredis';
 
 import { UsernameGeneratorModule } from './username-generator/username-generator.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), UsernameGeneratorModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    RedisModule.forRootAsync({
+      useFactory: (config: ConfigService) => ({
+        type: 'single',
+        url: config.getOrThrow<string>('REDIS_URL'),
+      }),
+      inject: [ConfigService],
+    }),
+    UsernameGeneratorModule,
+  ],
   controllers: [],
   providers: [],
 })
