37f9acb98a4040011dbe41abff4d0a36ea792c8c
AshedFox <a.mahn@mail.ru>
2025-08-23 23:02:29 +0300

    feat(web): add current user storage with context

---
+++ apps/web/src/features/auth/UserContext.tsx
@@ -0,0 +1,55 @@
+'use client';
+
+import { useQuery } from '@tanstack/react-query';
+import { createContext, ReactNode, useContext } from 'react';
+
+import { User } from '@/types/user';
+
+type UserContextValue = {
+  user: User | null;
+  isLoading: boolean;
+  refetchUser: () => Promise<void>;
+};
+
+const UserContext = createContext<UserContextValue | undefined>(undefined);
+
+type Props = { children: ReactNode; initialUser?: User | null };
+
+export function UserProvider({ children, initialUser = null }: Props) {
+  const {
+    data: user,
+    isLoading,
+    refetch,
+  } = useQuery<User>({
+    queryKey: ['current-user'],
+    queryFn: async () => {
+      const res = await fetch('/api/proxy/users/me', {
+        credentials: 'include',
+      });
+      if (!res.ok) {
+        throw new Error('Failed to get user!');
+      }
+      return res.json();
+    },
+    initialData: initialUser ?? undefined,
+    staleTime: 60000 * 10,
+  });
+
+  async function refetchUser() {
+    await refetch();
+  }
+
+  return (
+    <UserContext value={{ user: user ?? null, isLoading, refetchUser }}>
+      {children}
+    </UserContext>
+  );
+}
+
+export function useUser() {
+  const ctx = useContext(UserContext);
+  if (!ctx) {
+    throw new Error('useUser must be used within UserProvider');
+  }
+  return ctx;
+}
