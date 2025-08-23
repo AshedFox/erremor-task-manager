2dc99aadfa4ea057c792d532ed44e3ecd3c4ed61
AshedFox <a.mahn@mail.ru>
2025-08-24 00:29:35 +0300

    feat(web): add function to get current user server-side

---
+++ apps/web/src/lib/user.server.ts
@@ -0,0 +1,23 @@
+import { cookies } from 'next/headers';
+
+import { ACCESS_TOKEN_COOKIE_KEY, APP_BASE_URL } from '@/constants/env';
+import { User } from '@/types/user';
+
+export async function getServerUser() {
+  const cookieStore = await cookies();
+  const accessToken = cookieStore.get(ACCESS_TOKEN_COOKIE_KEY)?.value;
+  if (!accessToken) {
+    return null;
+  }
+
+  const res = await fetch(`${APP_BASE_URL}/api/proxy/users/me`, {
+    method: 'GET',
+    headers: { Authorization: `Bearer ${accessToken}` },
+  });
+
+  if (!res.ok) {
+    return null;
+  }
+
+  return (await res.json()) as User;
+}
