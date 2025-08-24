b2666142cf7abe95b17ef099e32bbdde96b31be7
AshedFox <a.mahn@mail.ru>
2025-08-24 21:49:20 +0300

    fix(web): allow getUser only server-side

--- apps/web/src/lib/get-user.server.ts
+++ apps/web/src/lib/get-user.server.ts
@@ -1,9 +1,11 @@
+import 'server-only';
+
 import { cookies } from 'next/headers';
 
 import { ACCESS_TOKEN_COOKIE_KEY, APP_BASE_URL } from '@/constants/env';
 import { User } from '@/types/user';
 
-export async function getServerUser() {
+export async function getUser() {
   const cookieStore = await cookies();
   const accessToken = cookieStore.get(ACCESS_TOKEN_COOKIE_KEY)?.value;
   if (!accessToken) {
