2f8e245d17277ddbb726b6c19a858197e7658309
AshedFox <a.mahn@mail.ru>
2025-08-24 21:51:53 +0300

    fix(web): remove passwordHash from user

--- apps/web/src/types/user.ts
+++ apps/web/src/types/user.ts
@@ -3,7 +3,6 @@ type AccountStatus = 'PENDING' | 'ACTIVE' | 'SUSPENDED' | 'ARCHIVED';
 export type User = {
   id: string;
   email: string;
-  passwordHash: string;
   status: AccountStatus;
   displayName?: string;
   username: string;
