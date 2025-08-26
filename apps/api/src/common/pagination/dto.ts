9064b37f0fb57dd86f53c60d593977bb4942081e
AshedFox <a.mahn@mail.ru>
2025-08-26 15:49:00 +0300

    feat(api): create dto and type for offset pagination

---
+++ apps/api/src/common/pagination/dto.ts
@@ -0,0 +1,27 @@
+import { Type } from 'class-transformer';
+import { IsInt, IsOptional, Max, Min } from 'class-validator';
+
+import { OffsetPagination } from './types';
+
+export class OffsetPaginationDto implements OffsetPagination {
+  @IsOptional()
+  @Type(() => Number)
+  @IsInt()
+  @Min(1)
+  page: number = 1;
+
+  @IsOptional()
+  @Type(() => Number)
+  @IsInt()
+  @Min(1)
+  @Max(1000)
+  limit: number = 20;
+
+  get skip(): number {
+    return (this.page - 1) * this.limit;
+  }
+
+  get take(): number {
+    return this.limit;
+  }
+}
