309f714d85d029e645b5bb91e625093d4a93d122
AshedFox <a.mahn@mail.ru>
2025-08-26 16:02:43 +0300

    feat(api): add projects controller

---
+++ apps/api/src/project/project.controller.ts
@@ -0,0 +1,79 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Query,
+  UseGuards,
+} from '@nestjs/common';
+import { ParticipantRole } from '@prisma/client';
+
+import { CurrentUser } from '@/auth/decorators/current-user.decorator';
+import { JwtAuthGuard } from '@/auth/guards/jwt-auth.guard';
+import { OffsetPaginationDto } from '@/common/pagination/dto';
+
+import { ProjectRole } from './decorators/project-roles.decorator';
+import { CreateProjectDto } from './dto/create-project.dto';
+import { ProjectsIncludeDto } from './dto/projects-include.dto';
+import { SearchProjectsFilterDto } from './dto/search-projects-filter.dto';
+import { SearchProjectsSortDto } from './dto/search-projects-sort.dto';
+import { UpdateProjectDto } from './dto/update-project.dto';
+import { ProjectRolesGuard } from './guards/project-roles.guard';
+import { ProjectService } from './project.service';
+
+@UseGuards(JwtAuthGuard)
+@Controller('projects')
+export class ProjectController {
+  constructor(private readonly projectService: ProjectService) {}
+
+  @Post()
+  create(
+    @Body() createProjectDto: CreateProjectDto,
+    @CurrentUser('sub') creatorId: string
+  ) {
+    return this.projectService.create({ ...createProjectDto, creatorId });
+  }
+
+  @Get()
+  search(
+    @CurrentUser('sub') userId: string,
+    @Query() pagination: OffsetPaginationDto,
+    @Query() filter: SearchProjectsFilterDto,
+    @Query() sort: SearchProjectsSortDto,
+    @Query() include: ProjectsIncludeDto
+  ) {
+    return this.projectService.search(
+      { skip: pagination.skip, take: pagination.take },
+      { ...filter, userId },
+      sort,
+      include
+    );
+  }
+
+  @UseGuards(ProjectRolesGuard)
+  @ProjectRole(ParticipantRole.GUEST)
+  @Get(':projectId')
+  findOne(@Param('projectId') id: string) {
+    return this.projectService.findOne(id);
+  }
+
+  @UseGuards(ProjectRolesGuard)
+  @ProjectRole(ParticipantRole.OWNER)
+  @Patch(':projectId')
+  update(
+    @Param('projectId') id: string,
+    @Body() updateProjectDto: UpdateProjectDto
+  ) {
+    return this.projectService.update(id, updateProjectDto);
+  }
+
+  @UseGuards(ProjectRolesGuard)
+  @ProjectRole(ParticipantRole.OWNER)
+  @Delete(':projectId')
+  remove(@Param('projectId') id: string) {
+    return this.projectService.remove(id);
+  }
+}
