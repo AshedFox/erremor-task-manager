89f0a55bf8053af3f2ca59b2a4ee85be592c8a57
AshedFox <a.mahn@mail.ru>
2025-08-26 15:58:32 +0300

    feat(api): implement projects service

---
+++ apps/api/src/project/project.service.ts
@@ -0,0 +1,85 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { ParticipantRole, Prisma, Project } from '@prisma/client';
+
+import { Include, mapInclude } from '@/common/include';
+import { OffsetPagination } from '@/common/pagination';
+import { Sort } from '@/common/sort';
+import { PrismaService } from '@/prisma/prisma.service';
+
+import {
+  CreateProjectParams,
+  FindManyProjectsFilter,
+  UpdateProjectParams,
+} from './types/project.types';
+
+@Injectable()
+export class ProjectService {
+  constructor(private readonly prisma: PrismaService) {}
+
+  create(data: CreateProjectParams): Promise<Project> {
+    return this.prisma.project.create({
+      data: {
+        ...data,
+        participants: {
+          create: {
+            role: ParticipantRole.OWNER,
+            userId: data.creatorId,
+          },
+        },
+      },
+    });
+  }
+
+  search(
+    pagination: OffsetPagination,
+    filter: FindManyProjectsFilter,
+    sort: Sort<Project>,
+    { include }: Include<Prisma.ProjectInclude>
+  ): Promise<Project[]> {
+    const prismaInclude = mapInclude(include);
+    const { search, userId, ...restFilter } = filter;
+    const where = {
+      OR: search
+        ? [
+            { name: { contains: search, mode: 'insensitive' } },
+            {
+              description: {
+                contains: search,
+                mode: 'insensitive',
+              },
+            },
+          ]
+        : undefined,
+      participants: { some: { userId } },
+      ...restFilter,
+    } satisfies Prisma.ProjectWhereInput;
+
+    return this.prisma.project.findMany({
+      ...pagination,
+      where,
+      orderBy: sort.sortBy ? { [sort.sortBy]: sort.sortOrder } : undefined,
+      include: prismaInclude,
+    });
+  }
+
+  async findOne(id: string): Promise<Project> {
+    const project = await this.prisma.project.findUnique({ where: { id } });
+
+    if (!project) {
+      throw new NotFoundException('Project not found!');
+    }
+
+    return project;
+  }
+
+  update(id: string, data: UpdateProjectParams): Promise<Project> {
+    return this.prisma.project.update({
+      where: { id },
+      data,
+    });
+  }
+
+  remove(id: string): Promise<Project> {
+    return this.prisma.project.delete({ where: { id } });
+  }
+}
