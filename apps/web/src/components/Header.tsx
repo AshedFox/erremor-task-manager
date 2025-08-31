ffaa107065ff6799ff583f66b601a2a08ae1c599
AshedFox <a.mahn@mail.ru>
2025-08-31 20:17:06 +0300

    feat(web): add search-bar to header component

--- apps/web/src/components/Header.tsx
+++ apps/web/src/components/Header.tsx
@@ -1,8 +1,8 @@
-import { Input } from '@workspace/ui/components/input';
 import { Separator } from '@workspace/ui/components/separator';
 import { SidebarTrigger } from '@workspace/ui/components/sidebar';
-import { SearchIcon } from 'lucide-react';
 import React from 'react';
+
+import SearchBar from './SearchBar';
 
 const Header = () => {
   return (
@@ -14,10 +14,7 @@ const Header = () => {
           className="mr-2 data-[orientation=vertical]:h-4"
         />
       </div>
-      <div className="relative max-w-lg flex-1 m-auto">
-        <SearchIcon className="absolute left-4 top-1/2 transform -translate-1/2 size-4 text-muted-foreground" />
-        <Input className="pl-10" placeholder="Search..." />
-      </div>
+      <SearchBar className="max-w-lg flex-1 m-auto" />
     </header>
   );
 };
