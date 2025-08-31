3ff0fee5aa5bd9a4d249aac397ab04ef121a5262
AshedFox <a.mahn@mail.ru>
2025-08-31 21:36:50 +0300

    feat(api): add to project controller method to get project tasks

--- apps/api/src/project/project.module.ts
+++ apps/api/src/project/project.module.ts
@@ -1,12 +1,13 @@
 import { Module } from '@nestjs/common';
 
 import { PrismaModule } from '@/prisma/prisma.module';
+import { TaskModule } from '@/task/task.module';
 
 import { ProjectController } from './project.controller';
 import { ProjectService } from './project.service';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, TaskModule],
   controllers: [ProjectController],
   providers: [ProjectService],
   exports: [ProjectService],
