d09c0b1267064a3b8a9fa7b940cf503e65266e27
AshedFox <a.mahn@mail.ru>
2025-09-02 14:38:54 +0300

    feat(api): add new types related to new pagination

--- apps/api/src/common/pagination/dto.ts
+++ apps/api/src/common/pagination/dto.ts
@@ -1,7 +1,15 @@
 import { Type } from 'class-transformer';
-import { IsInt, IsOptional, Max, Min } from 'class-validator';
+import {
+  IsInt,
+  IsOptional,
+  IsString,
+  Max,
+  Min,
+  ValidateIf,
+} from 'class-validator';
 
-import { OffsetPagination } from './types';
+import { Sort } from '../sort';
+import { CursorPagination, OffsetPagination } from './types';
 
 export class OffsetPaginationDto implements OffsetPagination {
   @IsOptional()
@@ -24,4 +32,64 @@ export class OffsetPaginationDto implements OffsetPagination {
   get take(): number {
     return this.limit;
   }
+}
+
+export class CursorPaginationDto implements CursorPagination {
+  @IsString()
+  cursor!: string;
+
+  @IsOptional()
+  @Type(() => Number)
+  @IsInt()
+  @Min(1)
+  @Max(1000)
+  limit: number = 20;
+
+  get take(): number {
+    return this.limit;
+  }
+}
+
+export class PaginationDto {
+  @ValidateIf((o) => (o as PaginationDto).skip === undefined)
+  @IsOptional()
+  @IsString()
+  cursor?: string;
+
+  @ValidateIf((o) => (o as PaginationDto).cursor === undefined)
+  @IsOptional()
+  @Type(() => Number)
+  @IsInt()
+  @Min(0)
+  page: number = 0;
+
+  @IsOptional()
+  @Type(() => Number)
+  @IsInt()
+  @Min(1)
+  @Max(1000)
+  limit: number = 20;
+
+  get skip(): number {
+    return this.page * this.limit;
+  }
+
+  get take(): number {
+    return this.limit;
+  }
+}
+
+export class Paginated<T> {
+  data!: T[];
+  meta!: PaginationMeta<T>;
+}
+
+export class PaginationMeta<T> {
+  totalCount?: number;
+  limit!: number;
+  currentPage?: number;
+  totalPages?: number;
+  hasNext!: boolean;
+  nextCursor?: string;
+  sort?: Sort<T>;
 }
--- apps/api/src/common/pagination/types.ts
+++ apps/api/src/common/pagination/types.ts
@@ -2,3 +2,12 @@ export type OffsetPagination = {
   take: number;
   skip: number;
 };
+
+export type CursorPagination = {
+  take: number;
+  cursor?: string;
+};
+
+export type Pagination =
+  | (CursorPagination & { skip?: never })
+  | (OffsetPagination & { cursor?: never });
