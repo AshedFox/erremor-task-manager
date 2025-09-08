c012f558b37b1578c81bd8561a50c21dc1b67fe3
AshedFox <a.mahn@mail.ru>
2025-09-08 17:20:31 +0300

    feat(web): add dialog for task with intercepting + parallel routes

---
+++ apps/web/src/features/task/TaskDialog.tsx
@@ -0,0 +1,41 @@
+'use client';
+
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+} from '@workspace/ui/components/dialog';
+import { useRouter } from 'next/navigation';
+import React from 'react';
+
+import { TaskWithInclude } from '@/types/task';
+
+import FullTaskCard from './FullTaskCard';
+
+type Props = {
+  task: TaskWithInclude<'tags' | 'creator'>;
+};
+
+const TaskDialog = ({ task }: Props) => {
+  const router = useRouter();
+  return (
+    <Dialog
+      onOpenChange={() => {
+        router.back();
+      }}
+      open={true}
+    >
+      <DialogContent className="max-h-[90dvh] overflow-y-auto">
+        <DialogHeader>
+          <DialogTitle>Task &quot;{task.title}&quot;</DialogTitle>
+          <DialogDescription>View task information</DialogDescription>
+        </DialogHeader>
+        <FullTaskCard task={task} />
+      </DialogContent>
+    </Dialog>
+  );
+};
+
+export default TaskDialog;
