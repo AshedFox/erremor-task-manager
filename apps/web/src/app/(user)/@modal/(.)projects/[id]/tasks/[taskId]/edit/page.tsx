c415f949f3b9c920f6e476f27573ccfae62f5d33
AshedFox <a.mahn@mail.ru>
2025-09-08 17:44:53 +0300

    feat(web): add dialog for task edit with combination of intercepting and parallel routes

---
+++ apps/web/src/app/(user)/@modal/(.)projects/[id]/tasks/[taskId]/edit/page.tsx
@@ -0,0 +1,37 @@
+import { Metadata } from 'next';
+import React from 'react';
+
+import EditTaskDialog from '@/features/task/EditTaskDialog';
+import { apiFetch } from '@/lib/api-fetch.server';
+import { Task, TaskWithInclude } from '@/types/task';
+
+type Props = {
+  params: Promise<{
+    id: string;
+    taskId: string;
+  }>;
+};
+
+export async function generateMetadata({ params }: Props): Promise<Metadata> {
+  const { taskId } = await params;
+  const task = await apiFetch<Task>(`/tasks/${taskId}?include=tags,creator`, {
+    next: { tags: [`tasks-${taskId}`] },
+  });
+
+  return {
+    title: `Edit Task "${task.title}"`,
+    description: task.description,
+  };
+}
+
+const Page = async ({ params }: Props) => {
+  const { taskId } = await params;
+  const task = await apiFetch<TaskWithInclude<'tags' | 'creator'>>(
+    `/tasks/${taskId}?include=tags,creator`,
+    { next: { tags: [`tasks-${taskId}`] } }
+  );
+
+  return <EditTaskDialog task={task} />;
+};
+
+export default Page;
