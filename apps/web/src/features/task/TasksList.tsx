aead644188650babd63ad66363a9516370442679
AshedFox <a.mahn@mail.ru>
2025-09-09 16:54:30 +0300

    feat(web): add virtualization to tasks list view

--- apps/web/src/features/task/TasksList.tsx
+++ apps/web/src/features/task/TasksList.tsx
@@ -1,9 +1,9 @@
 'use client';
 
 import { useSuspenseInfiniteQuery } from '@tanstack/react-query';
+import { useVirtualizer } from '@tanstack/react-virtual';
 import { Button } from '@workspace/ui/components/button';
-import { ScrollArea, ScrollBar } from '@workspace/ui/components/scroll-area';
-import React from 'react';
+import React, { useCallback, useMemo, useRef } from 'react';
 
 import Spinner from '@/components/Spinner';
 import { TASKS_PAGE_SIZE } from '@/constants/task';
@@ -29,39 +29,115 @@ const TasksList = ({ projectId }: Props) => {
       initialPageParam: 0,
     });
 
-  const items = data?.pages.flatMap((item) => item.data);
+  const items = useMemo(
+    () => data?.pages.flatMap((item) => item.data) || [],
+    [data?.pages]
+  );
+
+  const scrollElementRef = useRef<HTMLDivElement>(null);
+
+  const virtualizer = useVirtualizer({
+    count: items.length + (hasNextPage ? 1 : 0),
+    getScrollElement: () => scrollElementRef.current,
+    estimateSize: useCallback(
+      (index: number) => {
+        if (index === items.length) {
+          return 48;
+        }
+
+        return 200;
+      },
+      [items.length]
+    ),
+    overscan: 5,
+    getItemKey: useCallback(
+      (index: number) => {
+        if (index === items.length) {
+          return 'load-more-button';
+        }
+        return items[index]?.id || `item-${index}`;
+      },
+      [items]
+    ),
+  });
+
+  const virtualItems = virtualizer.getVirtualItems();
+
+  const handleLoadMore = useCallback(() => {
+    if (!isFetchingNextPage && hasNextPage) {
+      fetchNextPage();
+    }
+  }, [fetchNextPage, hasNextPage, isFetchingNextPage]);
+
+  if (items.length === 0) {
+    return (
+      <div className="flex-1 flex items-center justify-center text-muted-foreground ">
+        Nothing here...
+      </div>
+    );
+  }
 
   return (
-    <div className="flex flex-col gap-2 flex-1">
-      {items && items.length > 0 ? (
-        <ScrollArea className="flex flex-col gap-2">
-          <div className="flex flex-col gap-2">
-            {items.map((task) => (
-              <TaskCard key={task.id} task={task} />
-            ))}
-            {hasNextPage && (
-              <Button
-                disabled={isFetchingNextPage}
-                onClick={() => fetchNextPage()}
+    <div className="flex flex-col flex-1">
+      <div ref={scrollElementRef} className="flex-1 basis-0 overflow-auto p-4">
+        <div
+          className="relative w-full"
+          style={{
+            height: `${virtualizer.getTotalSize()}px`,
+          }}
+        >
+          {virtualItems.map((virtualItem) => {
+            const isLoadMoreButton = virtualItem.index === items.length;
+
+            if (isLoadMoreButton) {
+              return (
+                <div
+                  key={virtualItem.key}
+                  className="top-0 left-0 w-full absolute"
+                  style={{
+                    height: `${virtualItem.size}px`,
+                    transform: `translateY(${virtualItem.start}px)`,
+                  }}
+                >
+                  <Button
+                    disabled={isFetchingNextPage}
+                    onClick={handleLoadMore}
+                  >
+                    {isFetchingNextPage ? (
+                      <>
+                        <Spinner />
+                        Loading...
+                      </>
+                    ) : (
+                      'Load more'
+                    )}
+                  </Button>
+                </div>
+              );
+            }
+
+            const task = items[virtualItem.index];
+
+            if (!task) {
+              return null;
+            }
+
+            return (
+              <div
+                key={virtualItem.key}
+                data-index={virtualItem.index}
+                ref={virtualizer.measureElement}
+                style={{
+                  transform: `translateY(${virtualItem.start}px)`,
+                }}
+                className="pb-2 top-0 left-0 w-full absolute"
               >
-                {isFetchingNextPage ? (
-                  <>
-                    <Spinner />
-                    Loading...
-                  </>
-                ) : (
-                  'Load more'
-                )}
-              </Button>
-            )}
-          </div>
-          <ScrollBar />
-        </ScrollArea>
-      ) : (
-        <div className="flex-1 flex items-center justify-center text-muted-foreground ">
-          Nothing here...
+                <TaskCard task={task} />
+              </div>
+            );
+          })}
         </div>
-      )}
+      </div>
     </div>
   );
 };
