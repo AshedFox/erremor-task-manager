2392554fe9767cf4440a18e02cb3ff5b0fbcdd8f
AshedFox <a.mahn@mail.ru>
2025-09-09 17:53:08 +0300

    refactor(api): move user tasks search from user controller to user-task controller

--- apps/api/src/task/task.module.ts
+++ apps/api/src/task/task.module.ts
@@ -4,10 +4,11 @@ import { PrismaModule } from '@/prisma/prisma.module';
 
 import { TaskController } from './task.controller';
 import { TaskService } from './task.service';
+import { UserTaskController } from './user-task.controller';
 
 @Module({
   imports: [PrismaModule],
-  controllers: [TaskController],
+  controllers: [TaskController, UserTaskController],
   providers: [TaskService],
   exports: [TaskService],
 })
