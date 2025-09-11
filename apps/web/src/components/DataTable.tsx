6e86541965bd6b05e8998fc8af4fc5f0d75f2b37
AshedFox <a.mahn@mail.ru>
2025-09-11 23:58:22 +0300

    feat(web): add data table component with @tanstack/react-table

---
+++ apps/web/src/components/DataTable.tsx
@@ -0,0 +1,111 @@
+'use client';
+
+import {
+  ColumnDef,
+  flexRender,
+  getCoreRowModel,
+  useReactTable,
+} from '@tanstack/react-table';
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from '@workspace/ui/components/table';
+import React from 'react';
+
+import Pagination from './Pagination';
+
+type Props<TData, TValue> = {
+  columns: ColumnDef<TData, TValue>[];
+  data: TData[];
+  pageCount: number;
+  pageIndex: number;
+  pageSize: number;
+  onPageChange: (pageIndex: number) => void;
+};
+
+function DataTable<TData, TValue>({
+  columns,
+  data,
+  pageCount,
+  pageIndex,
+  pageSize,
+  onPageChange,
+}: Props<TData, TValue>) {
+  const table = useReactTable({
+    data,
+    columns,
+    getCoreRowModel: getCoreRowModel(),
+    pageCount,
+    state: {
+      pagination: { pageIndex, pageSize },
+    },
+    manualPagination: true,
+  });
+
+  return (
+    <div>
+      <div className="overflow-hidden rounded-md border">
+        <Table>
+          <TableHeader>
+            {table.getHeaderGroups().map((headerGroup) => (
+              <TableRow key={headerGroup.id}>
+                {headerGroup.headers.map((header) => {
+                  return (
+                    <TableHead key={header.id}>
+                      {header.isPlaceholder
+                        ? null
+                        : flexRender(
+                            header.column.columnDef.header,
+                            header.getContext()
+                          )}
+                    </TableHead>
+                  );
+                })}
+              </TableRow>
+            ))}
+          </TableHeader>
+          <TableBody>
+            {table.getRowModel().rows?.length ? (
+              table.getRowModel().rows.map((row) => (
+                <TableRow
+                  key={row.id}
+                  data-state={row.getIsSelected() && 'selected'}
+                >
+                  {row.getVisibleCells().map((cell) => (
+                    <TableCell key={cell.id}>
+                      {flexRender(
+                        cell.column.columnDef.cell,
+                        cell.getContext()
+                      )}
+                    </TableCell>
+                  ))}
+                </TableRow>
+              ))
+            ) : (
+              <TableRow>
+                <TableCell
+                  colSpan={columns.length}
+                  className="h-24 text-center"
+                >
+                  No results.
+                </TableCell>
+              </TableRow>
+            )}
+          </TableBody>
+        </Table>
+      </div>
+      <Pagination
+        className="px-2 mt-4"
+        pageCount={pageCount}
+        pageIndex={pageIndex}
+        onPageChange={onPageChange}
+      />
+    </div>
+  );
+}
+
+export default DataTable;
