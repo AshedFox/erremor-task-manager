294bbb62bec6a1421a32a9ecfa8204b3a04ef68f
AshedFox <a.mahn@mail.ru>
2025-09-12 15:57:07 +0300

    feat(web): implement invitation cancelation

---
+++ apps/web/src/lib/actions/cancel-invitation.ts
@@ -0,0 +1,24 @@
+'use server';
+
+import { revalidateTag } from 'next/cache';
+
+import { apiFetchSafe } from '../api-fetch.server';
+
+export async function cancelInvitation(projectId: string, userId: string) {
+  const result = await apiFetchSafe(
+    `/projects/${projectId}/invitations/${userId}`,
+    {
+      method: 'DELETE',
+      headers: {
+        'Content-Type': 'application/json',
+        Accept: 'application/json',
+      },
+    }
+  );
+
+  if (!result.error) {
+    revalidateTag(`project-${projectId}-invitations`);
+  }
+
+  return result;
+}
