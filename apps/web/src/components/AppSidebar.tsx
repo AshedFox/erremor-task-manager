e0f4df101ba9b889ff9954879f267dbf67fbce25
AshedFox <a.mahn@mail.ru>
2025-10-24 12:54:13 +0300

    feat(web): add recent project menu to app sidebar

--- apps/web/src/components/AppSidebar.tsx
+++ apps/web/src/components/AppSidebar.tsx
@@ -1,22 +1,38 @@
+import { dehydrate, HydrationBoundary } from '@tanstack/react-query';
 import {
   Sidebar,
   SidebarContent,
   SidebarFooter,
   SidebarGroup,
+  SidebarGroupContent,
   SidebarGroupLabel,
   SidebarHeader,
   SidebarSeparator,
 } from '@workspace/ui/components/sidebar';
 import Link from 'next/link';
-import React from 'react';
+import React, { Suspense } from 'react';
 
+import RecentProjectsSidebarMenu from '@/features/project/RecentProjectsSidebarGroup';
 import ProfileDropdownMenu from '@/features/user/ProfileDropdownMenu';
+import { apiFetch } from '@/lib/api-fetch.server';
+import { getQueryClient } from '@/lib/query-client';
+import { User } from '@/types/user';
 
 import Logo from './Logo';
 import SidebarNav from './SidebarNav';
+import Spinner from './Spinner';
 import ThemeSwitch from './ThemeSwitch';
 
-const AppSidebar = () => {
+const AppSidebar = async () => {
+  const queryClient = getQueryClient();
+
+  const user = await apiFetch<User>('/users/me');
+
+  await queryClient.prefetchQuery({
+    queryKey: ['projects', 'recent', user.id],
+    queryFn: () => apiFetch('/projects/recent?limit=5'),
+  });
+
   return (
     <Sidebar collapsible="icon">
       <SidebarHeader>
@@ -31,6 +47,13 @@ const AppSidebar = () => {
         <SidebarNav />
         <SidebarGroup>
           <SidebarGroupLabel>Recent projects</SidebarGroupLabel>
+          <SidebarGroupContent>
+            <HydrationBoundary state={dehydrate(queryClient)}>
+              <Suspense fallback={<Spinner />}>
+                <RecentProjectsSidebarMenu userId={user.id} />
+              </Suspense>
+            </HydrationBoundary>
+          </SidebarGroupContent>
         </SidebarGroup>
       </SidebarContent>
       <SidebarSeparator className="mx-auto" />
