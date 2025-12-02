dc86145ebf195f0f3ef088cbc801b5c4140fc71b
AshedFox <a.mahn@mail.ru>
2025-12-02 12:32:14 +0300

    fix(api): add project participant module import to support project roles guard

--- apps/api/src/project-invitation/project-invitation.module.ts
+++ apps/api/src/project-invitation/project-invitation.module.ts
@@ -3,13 +3,14 @@ import { Module } from '@nestjs/common';
 import { AuthModule } from '@/auth/auth.module';
 import { EmailModule } from '@/email/email.module';
 import { PrismaModule } from '@/prisma/prisma.module';
+import { ProjectParticipantModule } from '@/project-participant/project-participant.module';
 
 import { ProjectInvitationController } from './project-invitation.controller';
 import { ProjectInvitationService } from './project-invitation.service';
 import { UserInvitationController } from './user-invitation.controller';
 
 @Module({
-  imports: [PrismaModule, AuthModule, EmailModule],
+  imports: [PrismaModule, AuthModule, EmailModule, ProjectParticipantModule],
   controllers: [ProjectInvitationController, UserInvitationController],
   providers: [ProjectInvitationService],
   exports: [ProjectInvitationService],
--- apps/api/src/task/task.module.ts
+++ apps/api/src/task/task.module.ts
@@ -1,13 +1,14 @@
 import { Module } from '@nestjs/common';
 
 import { PrismaModule } from '@/prisma/prisma.module';
+import { ProjectParticipantModule } from '@/project-participant/project-participant.module';
 
 import { TaskController } from './task.controller';
 import { TaskService } from './task.service';
 import { UserTaskController } from './user-task.controller';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, ProjectParticipantModule],
   controllers: [TaskController, UserTaskController],
   providers: [TaskService],
   exports: [TaskService],
